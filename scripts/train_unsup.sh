bc5f073ddec91ca1326215b5982223ffa959360b
YiwuZhong <fmu2@wisc.edu>
2022-04-09 11:40:04 -0500

    update ncpu in scripts

--- scripts/train_unsup.sh
+++ scripts/train_unsup.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=12
 #SBATCH --time=96:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --partition=research
