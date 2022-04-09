cbc105b51f17b71d156f4b6ff4e122de3bb4d989
YiwuZhong <fmu2@wisc.edu>
2022-04-09 12:40:51 -0500

    update cfg

--- scripts/train_joint.sh
+++ scripts/train_joint.sh
@@ -8,10 +8,10 @@
 
 module load nvidia/cuda/11.3
 python setup.py build_ext --inplace
-python train_sup.py -c configs/sup/$1.yaml -n $2 -g $3
+python train_joint.py -c configs/joint/$1.yaml -n $2 -g $3
 
 # EXAMPLES:
-# sbatch --gres=gpu:1 --nodelist=euler20 train_sup.sh train_ch1_128 sup_ch1_128 0
-# sbatch --gres=gpu:2 --nodelist=euler19 train_sup.sh train_ch1_256 sup_ch1_256 0,1
+# sbatch --gres=gpu:1 --nodelist=euler20 train_joint.sh train_ch1_128 joint_ch1_128 0
+# sbatch --gres=gpu:2 --nodelist=euler19 train_joint.sh train_ch1_256 joint_ch1_256 0,1
 
 # NOTE: set $3 to 0 for 1 GPU, 0,1 for 2 GPUs, 0,1,2,3 for 4 GPUs, and so on.
